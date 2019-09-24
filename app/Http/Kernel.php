3b4cbd3c44d4a3632e31d90c1ef6f04d0fd3129c
Kevin Bell <kevin@webwaymaker.com>
2019-09-24 01:17:15 -0500

    Development

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,11 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+				\Illuminate\Routing\Middleware\SubstituteBindings::class,
+				
+				//Added to force HTTPS in production
+				//See https://stackoverflow.com/a/28403907
+				\App\Http\Middleware\HttpsProtocol::class
         ],
 
         'api' => [
